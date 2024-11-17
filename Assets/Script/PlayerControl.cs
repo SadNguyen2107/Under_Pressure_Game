08049310217269404f7a15a6404c55bb1902a36f
TGaDev203 <hoangthuychimte@gmail.com>
2024-11-17 09:47:03 +0700

    Add Assets

---
+++ Assets/Script/PlayerControl.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class GameMechanic : MonoBehaviour
+{
+    private void Awake()
+    {
+        InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+
+    }
+
+    private void Update()
+    {
+        
+    }
+}
